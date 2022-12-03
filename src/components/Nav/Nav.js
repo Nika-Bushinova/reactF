c1ac25e144541b88fef677516c80599014d1b04e
Nika-Bushinova <bushinovave@gmail.com>
2022-12-02 19:21:08 -0500

    fix:add messages

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Nav.module.css'
+
 //   <div className={`${classes.item} ${classes.active}`}><a href='#'>Profile</a></div>
-const Nav=()=>{//add classes
-   return(
-   <nav className={classes.wrapper__nav}>
-   <div className={classes.item}><a href='/profile'>Profile</a></div>
-   <div className={classes.item}><a href='/messages'>Messages</a></div>
-   <div className={classes.item}><a href='/news'>News</a></div>
-   <div className={classes.item}><a href='/music'>Music</a></div>
-   <div className={classes.item}><a href='/settings'>Settings</a></div>
-   </nav>)
+const Nav = () => {//add classes
+   return (
+      <nav className={classes.wrapper__nav}>
+         <div className={classes.item} ><NavLink to='/profile' className={nav => nav.isActive ? classes.active : classes.item}>Profile</NavLink></div>
+         <div className={classes.item}><NavLink to='/messages' className={nav => nav.isActive ? classes.active : classes.item}>Messages</NavLink></div>
+         <div className={classes.item}><NavLink to='/news' className={nav => nav.isActive ? classes.active : classes.item}>News</NavLink></div>
+         <div className={classes.item}><NavLink to='/music' className={nav => nav.isActive ? classes.active : classes.item}>Music</NavLink></div>
+         <div className={classes.item}><NavLink to='/settings' className={nav => nav.isActive ? classes.active : classes.item}>Settings</NavLink></div>
+      </nav>)
 }
 export default Nav
