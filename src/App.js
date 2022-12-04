64a1af672e18c1b99fc90fdcb4955700d7bb2945
Nika-Bushinova <bushinovave@gmail.com>
2022-12-04 17:41:47 -0500

    fix:add redux

--- src/App.js
+++ src/App.js
@@ -7,24 +7,24 @@ import Messages from './components/Messages/message/Messages'
 import News from './components/Nav/News';
 import Music from './components/Nav/Music';
 import Settings from './components/Nav/Settings';
-function App() {
+
+function App(props) {
+
   return (
     <BrowserRouter>
-  
     <div className="app-wrapper wrapper">
 <Header/>
-<Nav/>
+<Nav point={props.appState.sideBar}/>
 <div className='wrapper__content'>
 <Routes>
-<Route path='/profile' element={<Profile/>}/>
-<Route path='/messages/*' element={<Messages />}/>
+<Route path='/profile' element={<Profile arrLikes={props.appState.profilePage}/>}/>
+<Route path='/messages/*' element={<Messages messages={props.appState.dialogsPage} messagesData={props.appState.dialogsPage} /* messageArr={props.messageArr} dialogElements={props.dialogElements} *//>}/>
 <Route path='/news' element={<News />}/>
 <Route path='/music' element={<Music />}/>
 <Route path='/settings' element={<Settings />}/>
 </Routes>
 </div>
     </div>
-
     </BrowserRouter>
     
   );
---
+++ src/components/Messages/message/Message.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classes from '../Messages.module.css'
+const Message = (props) => {
+   
+   return (
+    <div className={classes.div__message}><div><img src={props.photo} className={classes.img__message}alt='friend'></img></div><div className={classes.message}>{props.text}</div></div>  
+   )
+}
+export default Message
