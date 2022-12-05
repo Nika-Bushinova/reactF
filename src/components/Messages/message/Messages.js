5680ee7302947a554d2c6d2ef756597c78404638
Nika-Bushinova <bushinovave@gmail.com>
2022-12-05 18:54:00 -0500

    fix:add function

--- src/components/Messages/message/Messages.js
+++ src/components/Messages/message/Messages.js
@@ -7,12 +7,14 @@ const MessageTitle = (props) => {
  
    return <div className={classes.center}><div><img src={props.photo} alt={props.name} className={classes.img__message}></img></div>Messages</div>
 }
-const NewMessage=()=>{
-   return (
-   <div className={classes.new}>New message<textarea></textarea></div>)
-}
+
+
 const Messages = (props) => {//add classes
-  
+   let textMessage=React.createRef()
+   let createMessage=()=>{
+      let text=textMessage.current.value;
+      alert(text)
+   }
 let dialogElements = props.messagesData.arr.map((el) => { return <DialogItem name={el.name} id={el.id} photo={el.photo} /> })
 let messageArr = props.messagesData.messagesData.map((el) => { return <Message text={el.message} id={el.id} photo={dialogElements[el.id].props.photo}/> })
    return (
@@ -20,7 +22,10 @@ let messageArr = props.messagesData.messagesData.map((el) => { return <Message t
          <div className={classes.dialogs__messages}>
             <MessageTitle photo={messageArr[0].props.photo} />
           {messageArr}
-           <NewMessage/>
+
+           <div className={classes.new}>New message
+           <textarea ref={textMessage}></textarea></div>
+           <div onClick={createMessage}>Send</div>
          </div>
          <div className={classes.dialogs__names}>
             <div>Dialogs</div>
