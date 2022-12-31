292b5202c455f8268346f3aa6cb9d8df90079093
Nika-Bushinova <bushinovave@gmail.com>
2022-12-31 06:35:56 -0500

    fix:add getUsersThunkCreator to UsersReducer

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,11 +1,11 @@
 
-import { combineReducers, createStore} from "redux";
+import { applyMiddleware, combineReducers, createStore} from "redux";
 import profileReducer from './profileReducer';
 import { messageReducer } from './MessageReduser';
 import sideBarReducer from './sideBar';
 import usersReducer from "./UsersReducer";
 import authReducer from "./authReducer";
-
+import  thunkMiddleware  from "redux-thunk";
 
 let reducers =combineReducers({
    profilePage:profileReducer,
@@ -14,7 +14,7 @@ let reducers =combineReducers({
   usersPage:usersReducer,
   auth:authReducer
 })
-let store=createStore(reducers)
+let store=createStore(reducers, applyMiddleware(thunkMiddleware))
 window.store=store
 export default store;
 
