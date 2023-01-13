3f6555fc7df65f46f962229393990557cc91e407
Nika-Bushinova <bushinovave@gmail.com>
2023-01-13 18:31:07 -0500

    fix:add style Error for posts

--- src/components/validators/validator.js
+++ src/components/validators/validator.js
@@ -1,6 +1,6 @@
 export const required=(value)=>{
    if (value) return undefined
-   return 'error message'
+   return 'Field is required'
 }
 export const maxLengthF=(symbolsCount)=>(value)=>{
    if (value&&value.length>symbolsCount) return `Max length is ${symbolsCount} symbols`
